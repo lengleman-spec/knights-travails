9096bc5cdbea99eacf7b56d6319206930b58c60e
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-28 11:07:27 -0400

    Create function to get and set the coordinates to the board.

---
+++ search-algo.js
@@ -0,0 +1,9 @@
+// Using a breadth-first algo for moves
+const squareRegistry = new Map(); //Map stores key-value pairs and remembers the original insertion order
+
+// Get/Set current coords to the board
+const chessSquare = (x, y) => {
+  const xPosition = x;
+  const yPosition = y;
+  let predecessor;
+};
