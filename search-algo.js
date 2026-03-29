0e66a18e61c5619c4419945297f02f18a6664609
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-29 09:21:30 -0400

    Add logic to confirm valid moves, must be between 0 - 8. Calculate new set of coordinates against offsets.

--- search-algo.js
+++ search-algo.js
@@ -18,4 +18,29 @@ const chessSquare = (x, y) => {
     [-2, 1],
     [-2, -1],
   ];
+
+  const getPredecessor = () => predecessor;
+  const setPredecessor = (newPredecessor) => {
+    predecessor = predecessor || newPredecessor;
+  };
+
+  // Show x y coordinates:
+  const name = () => `${x}, ${y}`;
+
+  // Evaluate current possible moves against offsets
+  const possibleKnightMoves = () => {
+    return KNIGHT_MOVES.map((offset) =>
+      newSquareFrom(offset[0], offset[1]),
+    ).filter((square) => square !== undefined);
+  };
+
+  // Calculate new set of square coords against offsets
+  const newSquareFrom = (xOffset, yOffset) => {
+    const [newX, newY] = [xPosition + xOffset, yPosition + yOffset];
+
+    // If offset is between 0 and 8, it's a valid move:
+    if (0 <= newX && newX < 8 && 0 <= newY && y < 8) {
+      return chessSquare(newX, newY);
+    }
+  };
 };
