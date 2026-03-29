1b25943d081d692688c31b3a0777d120cf5d73db
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-29 11:16:20 -0400

    Create logic to make knight image move to different cells.

--- ui-move.js
+++ ui-move.js
@@ -14,6 +14,28 @@ const uiController = () => {
     cellNode.addEventListener("click", function () {
       let clickedLocation = JSON.parse("[" + cellNode.dataset.coordArray + "]");
       console.log("Clicked on location ", clickedLocation);
+
+      //Loop again to locate new current location and assign coords.
+      // Remove old knight img from previous coords
+      // Call knightsTravails module to start search algo
+      const cellNodes = document.querySelectorAll("td");
+      cellNodes.forEach((cellNode) => {
+        if (cellNode.querySelector("img") !== null) {
+          let knightLocation = JSON.parse(
+            "[" + cellNode.dataset.coordArray + "]",
+          );
+          console.log("Current knight location ", knightLocation);
+          const knightImg = document.querySelector("img");
+          knightImg.remove();
+          knightsTravails(knightLocation, clickedLocation);
+        }
+      });
+
+      // Create the new knight img at the clicked location
+      const knightImg = document.createElement("img");
+      knightImg.src =
+        "https://thumbs.dreamstime.com/b/white-chess-knight-piece-background-icon-203800607.jpg";
+      cellNode.appendChild(knightImg);
     });
   });
 };
