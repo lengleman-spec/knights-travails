8cf32279238dcb51e05e358c50ffd026eb322406
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-29 10:15:13 -0400

    Create start and end nodes and initialize BFS queue. Get all possible knight moves and record how each square was reached. Add new squares to the queue until the end target is reached.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import { gameboard } from "./gameboard.js";
+import { knightsTravails } from "./search-algo.js";
 
 // DOM for chess board module
-const appController = function () {
-  gameboard();
-};
 
-gameboard();
+const appController = (function () {
+  gameboard();
+  knightsTravails([3, 3], [4, 3]);
+})();
--- search-algo.js
+++ search-algo.js
@@ -29,9 +29,9 @@ const chessSquare = (x, y) => {
 
   // Evaluate current possible moves against offsets
   const possibleKnightMoves = () => {
-    return KNIGHT_MOVES.map((offset) =>
-      newSquareFrom(offset[0], offset[1]),
-    ).filter((square) => square !== undefined);
+    return knightMoves
+      .map((offset) => newSquareFrom(offset[0], offset[1]))
+      .filter((square) => square !== undefined);
   };
 
   // Calculate new set of square coords against offsets
@@ -39,8 +39,53 @@ const chessSquare = (x, y) => {
     const [newX, newY] = [xPosition + xOffset, yPosition + yOffset];
 
     // If offset is between 0 and 8, it's a valid move:
-    if (0 <= newX && newX < 8 && 0 <= newY && y < 8) {
+    if (0 <= newX && newX < 8 && 0 <= newY && newY < 8) {
       return chessSquare(newX, newY);
     }
   };
+
+  // Get/set map constructor object name
+  // if squareRegistry does not have a name, create one and give it the following objects
+  if (squareRegistry.has(name())) {
+    return squareRegistry.get(name());
+  } else {
+    const newSquare = {
+      name,
+      getPredecessor,
+      setPredecessor,
+      possibleKnightMoves,
+    };
+
+    squareRegistry.set(name(), newSquare);
+    return newSquare;
+  }
 };
+
+// Intake the click coords from the user and run the search algo
+const knightsTravails = (start, finish) => {
+  squareRegistry.clear(); // clear old values
+
+  const origin = chessSquare(...start);
+  const target = chessSquare(...finish);
+
+  const queue = [origin];
+  while (!queue.includes(target)) {
+    const currentSquare = queue.shift();
+
+    const enqueueList = currentSquare.possibleKnightMoves();
+    enqueueList.forEach((square) => square.setPredecessor(currentSquare));
+    queue.push(...enqueueList);
+  }
+
+  const path = [target];
+  while (!path.includes(origin)) {
+    const prevSquare = path[0].getPredecessor();
+    path.unshift(prevSquare);
+  }
+
+  console.log(`The shortest path was ${path.length - 1} moves!`);
+  console.log("The moves were:");
+  path.forEach((square) => console.log(square.name()));
+};
+
+export { knightsTravails };
